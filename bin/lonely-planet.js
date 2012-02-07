cca0aaf26fbf1af514f30bfa222bbb9c6d925c2b
Adel Smee <adel.smee@gmail.com>
2012-02-07 19:51:48 +1100

    first code commmit

---
+++ bin/lonely-planet.js
@@ -0,0 +1,23 @@
+// Lonely-Planet.js 1.0
+// (c) 2012 Adel Smee, Pearshaped Development Inc.
+
+// The  New Planet module that encapsulates this application.
+function newPlanet() {
+	// Includes
+	var newParser = require('../lib/parser.js'),
+	// Arguments: taxonomy file, destination file, output directory
+	// Ignore first two arguments as they are loc of node and current file
+		planetArgs = process.argv.splice(2);
+
+	return {
+		
+		parseXML: function(){
+			var parser = newParser(planetArgs[0], planetArgs[1]);
+			parser.parseXml2Html();
+		}
+
+	};
+}
+
+var planet = newPlanet();
+planet.parseXML();
